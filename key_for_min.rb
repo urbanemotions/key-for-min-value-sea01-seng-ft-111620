537627ef374a4331d54f89d8d0686ba6622ecada
Sin Wildy <sw.wildy@gmail.com>
2020-11-05 05:44:22 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,18 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  answer = []  
+  if name_hash.to_a == []
+    return nil
+  else
+    min_value = name_hash.to_a[0][1]
+  end 
+  
+  name_hash.each do |name, value|
+    if min_value >= value
+      min_value = value
+      answer = name
+    end
+  end
+  answer
 end
