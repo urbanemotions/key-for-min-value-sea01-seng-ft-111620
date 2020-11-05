8be5a9f4a86e367398f9a9377b66eea5a7ecfa12
Sin Wildy <sw.wildy@gmail.com>
2020-11-05 05:14:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,12 +2,22 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  
   min_value = 1000
+  name_hash.select {|name, value| value}
   name_hash.each do |name, value|
-    if min_value > value
-      min_value = value
-      key_min_value = name
+    if name_hash[value] < min_value
+      min_value = name_hash[value]
+      answer_min_value = name_hash{name]
     end
   end
-  key_min_value
+  answer_min_value
+  
+#  name_hash.each do |name, value|
+#    if min_value > value
+#      min_value = value
+#      key_min_value = name
+#    end
+#  end
+#  key_min_value
 end
