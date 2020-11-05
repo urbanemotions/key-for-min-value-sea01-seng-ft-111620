5798dc251afcae4942ba8b195a3cd6938716a068
Sin Wildy <sw.wildy@gmail.com>
2020-11-05 05:16:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,12 +6,12 @@ def key_for_min_value(name_hash)
   min_value = 1000
   name_hash.select {|name, value| value}
   name_hash.each do |name, value|
-    if name_hash[value] < min_value
+    if min_value > name_hash[value]
       min_value = name_hash[value]
-      answer_min_value = name_hash[name]
+      answer = name_hash[name]
     end
   end
-  answer_min_value
+  answer
   
 #  name_hash.each do |name, value|
 #    if min_value > value
