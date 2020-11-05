2bc79dfac097e0ec928d6bbc53f331d293e384b4
Sin Wildy <sw.wildy@gmail.com>
2020-11-05 05:35:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  answer = nil 
+ # answer = nil 
   if name_hash.to_a == []
     return nil
   else
@@ -16,12 +16,4 @@ def key_for_min_value(name_hash)
     end
   end
   answer
-  
-#  name_hash.each do |name, value|
-#    if min_value > value
-#      min_value = value
-#      key_min_value = name
-#    end
-#  end
-#  key_min_value
 end
